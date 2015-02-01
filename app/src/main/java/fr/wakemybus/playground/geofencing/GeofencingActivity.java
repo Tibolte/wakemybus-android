50f6b6ea436c743e68717abd10903a1efc48d1ac
Thibault Guegan <thibault.guegan@gmail.com>
2015-02-01 15:25:09 +0000

    Set-up draggable marker to select lat/lon

--- app/src/main/java/fr/wakemybus/playground/geofencing/GeofencingActivity.java
+++ app/src/main/java/fr/wakemybus/playground/geofencing/GeofencingActivity.java
@@ -1,18 +1,78 @@
 package fr.wakemybus.playground.geofencing;
 
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import fr.wakemybus.wakemybus.R;
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
+import rx.functions.Action1;
 
 public class GeofencingActivity extends ActionBarActivity {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geofencing);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(getApplicationContext());
+        locationProvider.getLastKnownLocation()
+                .subscribe(new Action1<Location>() {
+                    @Override
+                    public void call(Location location) {
+                        if (map!=null){
+                            double curLat = location.getLatitude();
+                            double curLon = location.getLongitude();
+                            LatLng currentPos = new LatLng(curLat, curLon);
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(curLat, curLon), 12));
+
+                            final Marker marker= map.addMarker(new MarkerOptions().position(currentPos)
+                                    .title("Draggable Marker")
+                                    .snippet("Long press and move the marker if needed.")
+                                    .draggable(true));
+                            map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+                                @Override
+                                public void onMarkerDrag(Marker arg0) {
+                                    // TODO Auto-generated method stub
+                                    Log.d("Marker", "Dragging");
+                                }
+
+                                @Override
+                                public void onMarkerDragEnd(Marker arg0) {
+                                    // TODO Auto-generated method stub
+                                    LatLng markerLocation = marker.getPosition();
+                                    Toast.makeText(GeofencingActivity.this, markerLocation.toString(), Toast.LENGTH_LONG).show();
+                                    Log.d("Marker", "finished");
+                                }
+
+                                @Override
+                                public void onMarkerDragStart(Marker arg0) {
+                                    // TODO Auto-generated method stub
+                                    Log.d("Marker", "Started");
+
+                                }
+                            });
+                        }
+                    }
+                });
+
     }
 
 
